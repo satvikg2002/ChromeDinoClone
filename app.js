99467c660e38edc4f2ddb49807a4952b4f34d813
Satvik Gupta <satvik.noida@gmail.com>
2022-01-12 12:04:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const score = document.getElementById("score");
+
+let isAlive = setInterval(() => {
+    // current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    // current cactus X position
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // to detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert("Game Over");
+        score.textContent = "0";
+    }
+}, 10);
+
+let addScore = setInterval(() => {
+    let currScore = parseInt(score.textContent);
+    currScore += 1;
+    score.textContent = currScore;
+}, 40)
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump")
+        }, 300)
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 32)
+        jump();
+});
